a2f69e64425ce762eecdf4435940c8de271974bd
Manoj Bhadugale <manojbhadugale@hotmail.com>
2021-04-11 20:06:31 +0530

    Graphs added

--- FogNode/FogNodeForm.Designer.cs
+++ FogNode/FogNodeForm.Designer.cs
@@ -28,6 +28,13 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
 			this.textToken = new System.Windows.Forms.TextBox();
@@ -43,8 +50,12 @@
 			this.label5 = new System.Windows.Forms.Label();
 			this.labelBPM = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
+			this.chartPulse = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.chartTemp = new System.Windows.Forms.DataVisualization.Charting.Chart();
 			this.panel1.SuspendLayout();
 			this.panel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.chartPulse)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.chartTemp)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -53,32 +64,36 @@
 			this.label1.Dock = System.Windows.Forms.DockStyle.Top;
 			this.label1.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label1.Location = new System.Drawing.Point(0, 0);
+			this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(425, 40);
+			this.label1.Size = new System.Drawing.Size(348, 32);
 			this.label1.TabIndex = 0;
 			this.label1.Text = "Patient Health Monitoring";
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(13, 59);
+			this.label2.Location = new System.Drawing.Point(10, 48);
+			this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(86, 17);
+			this.label2.Size = new System.Drawing.Size(66, 13);
 			this.label2.TabIndex = 1;
 			this.label2.Text = "Enter Token";
 			// 
 			// textToken
 			// 
-			this.textToken.Location = new System.Drawing.Point(105, 56);
+			this.textToken.Location = new System.Drawing.Point(79, 46);
+			this.textToken.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.textToken.Name = "textToken";
-			this.textToken.Size = new System.Drawing.Size(321, 22);
+			this.textToken.Size = new System.Drawing.Size(242, 20);
 			this.textToken.TabIndex = 2;
 			// 
 			// buttonStart
 			// 
-			this.buttonStart.Location = new System.Drawing.Point(432, 56);
+			this.buttonStart.Location = new System.Drawing.Point(324, 46);
+			this.buttonStart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.buttonStart.Name = "buttonStart";
-			this.buttonStart.Size = new System.Drawing.Size(75, 23);
+			this.buttonStart.Size = new System.Drawing.Size(56, 19);
 			this.buttonStart.TabIndex = 3;
 			this.buttonStart.Text = "Start";
 			this.buttonStart.UseVisualStyleBackColor = true;
@@ -95,48 +110,54 @@
 			this.panel1.Controls.Add(this.label2);
 			this.panel1.Controls.Add(this.buttonStart);
 			this.panel1.Controls.Add(this.textToken);
-			this.panel1.Location = new System.Drawing.Point(12, 43);
+			this.panel1.Location = new System.Drawing.Point(9, 35);
+			this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(860, 96);
+			this.panel1.Size = new System.Drawing.Size(646, 78);
 			this.panel1.TabIndex = 4;
 			// 
 			// textBoxPort
 			// 
-			this.textBoxPort.Location = new System.Drawing.Point(392, 14);
+			this.textBoxPort.Location = new System.Drawing.Point(294, 11);
+			this.textBoxPort.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.textBoxPort.Name = "textBoxPort";
-			this.textBoxPort.Size = new System.Drawing.Size(171, 22);
+			this.textBoxPort.Size = new System.Drawing.Size(129, 20);
 			this.textBoxPort.TabIndex = 8;
 			// 
 			// label8
 			// 
 			this.label8.AutoSize = true;
-			this.label8.Location = new System.Drawing.Point(306, 17);
+			this.label8.Location = new System.Drawing.Point(230, 14);
+			this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.label8.Name = "label8";
-			this.label8.Size = new System.Drawing.Size(80, 17);
+			this.label8.Size = new System.Drawing.Size(60, 13);
 			this.label8.TabIndex = 7;
 			this.label8.Text = "Server Port";
 			// 
 			// textBoxIP
 			// 
-			this.textBoxIP.Location = new System.Drawing.Point(125, 14);
+			this.textBoxIP.Location = new System.Drawing.Point(94, 11);
+			this.textBoxIP.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.textBoxIP.Name = "textBoxIP";
-			this.textBoxIP.Size = new System.Drawing.Size(171, 22);
+			this.textBoxIP.Size = new System.Drawing.Size(129, 20);
 			this.textBoxIP.TabIndex = 6;
 			// 
 			// label7
 			// 
 			this.label7.AutoSize = true;
-			this.label7.Location = new System.Drawing.Point(13, 17);
+			this.label7.Location = new System.Drawing.Point(10, 14);
+			this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.label7.Name = "label7";
-			this.label7.Size = new System.Drawing.Size(106, 17);
+			this.label7.Size = new System.Drawing.Size(79, 13);
 			this.label7.TabIndex = 5;
 			this.label7.Text = "Server Address";
 			// 
 			// buttonStop
 			// 
-			this.buttonStop.Location = new System.Drawing.Point(513, 56);
+			this.buttonStop.Location = new System.Drawing.Point(385, 46);
+			this.buttonStop.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.buttonStop.Name = "buttonStop";
-			this.buttonStop.Size = new System.Drawing.Size(75, 23);
+			this.buttonStop.Size = new System.Drawing.Size(56, 19);
 			this.buttonStop.TabIndex = 4;
 			this.buttonStop.Text = "Stop";
 			this.buttonStop.UseVisualStyleBackColor = true;
@@ -149,9 +170,10 @@
 			this.panel2.Controls.Add(this.label5);
 			this.panel2.Controls.Add(this.labelBPM);
 			this.panel2.Controls.Add(this.label3);
-			this.panel2.Location = new System.Drawing.Point(12, 145);
+			this.panel2.Location = new System.Drawing.Point(9, 118);
+			this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.panel2.Name = "panel2";
-			this.panel2.Size = new System.Drawing.Size(860, 134);
+			this.panel2.Size = new System.Drawing.Size(646, 109);
 			this.panel2.TabIndex = 5;
 			// 
 			// labelTemp
@@ -159,19 +181,21 @@
 			this.labelTemp.AutoSize = true;
 			this.labelTemp.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
 			this.labelTemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.labelTemp.Location = new System.Drawing.Point(282, 64);
-			this.labelTemp.MinimumSize = new System.Drawing.Size(200, 0);
+			this.labelTemp.Location = new System.Drawing.Point(212, 52);
+			this.labelTemp.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.labelTemp.MinimumSize = new System.Drawing.Size(150, 0);
 			this.labelTemp.Name = "labelTemp";
-			this.labelTemp.Size = new System.Drawing.Size(200, 48);
+			this.labelTemp.Size = new System.Drawing.Size(150, 39);
 			this.labelTemp.TabIndex = 3;
 			// 
 			// label5
 			// 
 			this.label5.AutoSize = true;
 			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label5.Location = new System.Drawing.Point(8, 64);
+			this.label5.Location = new System.Drawing.Point(6, 52);
+			this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(257, 46);
+			this.label5.Size = new System.Drawing.Size(211, 37);
 			this.label5.TabIndex = 2;
 			this.label5.Text = "Temperature";
 			// 
@@ -180,37 +204,84 @@
 			this.labelBPM.AutoSize = true;
 			this.labelBPM.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
 			this.labelBPM.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.labelBPM.Location = new System.Drawing.Point(282, 9);
-			this.labelBPM.MinimumSize = new System.Drawing.Size(200, 0);
+			this.labelBPM.Location = new System.Drawing.Point(212, 7);
+			this.labelBPM.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.labelBPM.MinimumSize = new System.Drawing.Size(150, 0);
 			this.labelBPM.Name = "labelBPM";
-			this.labelBPM.Size = new System.Drawing.Size(200, 48);
+			this.labelBPM.Size = new System.Drawing.Size(150, 39);
 			this.labelBPM.TabIndex = 1;
 			// 
 			// label3
 			// 
 			this.label3.AutoSize = true;
 			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label3.Location = new System.Drawing.Point(8, 9);
+			this.label3.Location = new System.Drawing.Point(6, 7);
+			this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(225, 46);
+			this.label3.Size = new System.Drawing.Size(181, 37);
 			this.label3.TabIndex = 0;
 			this.label3.Text = "Pulse Rate";
 			// 
+			// chartPulse
+			// 
+			chartArea1.Name = "ChartArea1";
+			this.chartPulse.ChartAreas.Add(chartArea1);
+			legend1.Enabled = false;
+			legend1.Name = "Legend1";
+			this.chartPulse.Legends.Add(legend1);
+			this.chartPulse.Location = new System.Drawing.Point(12, 232);
+			this.chartPulse.Name = "chartPulse";
+			series1.ChartArea = "ChartArea1";
+			series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+			series1.Color = System.Drawing.Color.Black;
+			series1.Legend = "Legend1";
+			series1.Name = "Series1";
+			this.chartPulse.Series.Add(series1);
+			this.chartPulse.Size = new System.Drawing.Size(639, 197);
+			this.chartPulse.TabIndex = 6;
+			this.chartPulse.Text = "chart1";
+			title1.Name = "Pulse Rate";
+			this.chartPulse.Titles.Add(title1);
+			// 
+			// chartTemp
+			// 
+			chartArea2.Name = "ChartArea1";
+			this.chartTemp.ChartAreas.Add(chartArea2);
+			legend2.Enabled = false;
+			legend2.Name = "Legend1";
+			this.chartTemp.Legends.Add(legend2);
+			this.chartTemp.Location = new System.Drawing.Point(12, 435);
+			this.chartTemp.Name = "chartTemp";
+			series2.ChartArea = "ChartArea1";
+			series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+			series2.Color = System.Drawing.Color.Red;
+			series2.Legend = "Legend1";
+			series2.Name = "Series1";
+			this.chartTemp.Series.Add(series2);
+			this.chartTemp.Size = new System.Drawing.Size(643, 204);
+			this.chartTemp.TabIndex = 7;
+			this.chartTemp.Text = "chart2";
+			// 
 			// FogNodeForm
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(884, 292);
+			this.ClientSize = new System.Drawing.Size(663, 651);
+			this.Controls.Add(this.chartTemp);
+			this.Controls.Add(this.chartPulse);
 			this.Controls.Add(this.panel2);
 			this.Controls.Add(this.panel1);
 			this.Controls.Add(this.label1);
-			this.MinimumSize = new System.Drawing.Size(200, 47);
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.MinimumSize = new System.Drawing.Size(153, 45);
 			this.Name = "FogNodeForm";
 			this.Text = "FogNodeForm";
 			this.panel1.ResumeLayout(false);
 			this.panel1.PerformLayout();
 			this.panel2.ResumeLayout(false);
 			this.panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.chartPulse)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.chartTemp)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -233,5 +304,7 @@
 		private System.Windows.Forms.Label label8;
 		private System.Windows.Forms.TextBox textBoxIP;
 		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chartPulse;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chartTemp;
 	}
 }
--- FogNode/FogNodeForm.cs
+++ FogNode/FogNodeForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -17,20 +18,29 @@ namespace FogNode
 		private bool running = false;
 		private String BASE_URL;
 		private String TOKEN;
+		private int count = 0;
+
+		private bool isSimulation = true;
 
 		public FogNodeForm()
 		{
 			InitializeComponent();
 			buttonStart.Enabled = true;
 			buttonStop.Enabled = false;
+
+			chartPulse.ChartAreas[0].AxisY.Minimum = 40;
+			chartTemp.ChartAreas[0].AxisY.Minimum = 90;
+
+			chartPulse.Titles.Add("Pulse Rate");
+			chartTemp.Titles.Add("Temperature");
 		}
 
 		private void buttonStart_Click(object sender, EventArgs e)
 		{
-			BASE_URL = "http://"+textBoxIP.Text+":"+textBoxPort.Text+"/api";
+			BASE_URL = "http://" + textBoxIP.Text + ":" + textBoxPort.Text + "/api";
 			TOKEN = textToken.Text;
 			buttonStop.Enabled = true;
-			buttonStart.Enabled=false;
+			buttonStart.Enabled = false;
 			running = true;
 			System.Threading.Thread t = new System.Threading.Thread(new System.Threading.ThreadStart(MonitorThread));
 			t.Start();
@@ -41,24 +51,45 @@ namespace FogNode
 			HttpClient client = new HttpClient();
 			Random random = new Random();
 
+
 			while (running)
 			{
 				try
 				{
 					Console.WriteLine("Reading values");
-					Thread.Sleep(60000);
+					Thread.Sleep(1000);
 
-					int bpm = random.Next(60, 100);
-					float temp = random.Next(9500, 10100) / 100.0f;
+					int bpm;
+					float temp;
+
+					if (isSimulation)
+					{
+						bpm = random.Next(60, 100);
+						temp = random.Next(9500, 10100) / 100.0f;
+					}
+					else
+					{
+
+						Object[] values = ReadValues();
+						bpm = (int)values[0];
+						temp = (float)values[1];
+					}
 
 					int isnormal = 1;
-					if (bpm < 60 || bpm>100 || temp<95 || temp>100)
+					if (bpm < 60 || bpm > 100 || temp < 95 || temp > 100)
 					{
 						isnormal = 0;
 					}
 
 					labelBPM.Text = bpm.ToString();
 					labelTemp.Text = temp.ToString();
+
+					chartPulse.Series[0].Points.AddXY(count++, bpm);
+					chartTemp.Series[0].Points.AddXY(count++, temp);
+
+					chartPulse.ChartAreas[0].AxisX.Minimum = Math.Max(count - 50, 0);
+					chartTemp.ChartAreas[0].AxisX.Minimum = Math.Max(count - 50, 0);
+
 
 					String urlpart = TOKEN + "_" + bpm + "_" + temp + "_" + isnormal;
 					urlpart = urlpart.Replace(".", "__");
@@ -70,7 +101,7 @@ namespace FogNode
 					String resulttext = await result.Content.ReadAsStringAsync();
 					Console.WriteLine(resulttext);
 				}
-				catch(Exception e)
+				catch (Exception e)
 				{
 					MessageBox.Show(e.Message);
 					running = false;
@@ -85,6 +116,30 @@ namespace FogNode
 			running = false;
 			buttonStart.Enabled = true;
 			buttonStop.Enabled = false;
+		}
+
+		public static Object[] ReadValues()
+		{
+			using (SerialPort port = new SerialPort("COM5", 9600, Parity.None, 8, StopBits.One))
+			{
+				try
+				{
+					port.Open();
+
+					string data = port.ReadLine();
+
+					string value1 = data.Substring(5, 2);   // temp value
+					string value2 = data.Substring(11, 2);     // heartbeat value
+
+					return new object[] { int.Parse(value2), float.Parse(value1) };
+
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.Message);
+					return new object[] { 0, 0f };
+				}
+			}
 		}
 	}
 }
